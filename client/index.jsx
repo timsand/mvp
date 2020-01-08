a838b1f0fca8ac3aecbc5c5d89439368fd62ee70
Tim Sanderson <twsand11@gmail.com>
2020-01-08 11:04:07 -0600

    Basic redux boilerplate

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app.jsx";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { masterReducer } from "./reducers/masterReducer.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  masterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ client/reducers/masterReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+  name: "Grog"
+};
+
+export function masterReducer(state = initialState, action) {
+  return state;
+}

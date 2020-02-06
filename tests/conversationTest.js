c5e1733250a9de489a644bf6ac9375f1d4df4dca
Tim Sanderson <twsand11@gmail.com>
2020-02-06 14:04:51 -0600

    Basic tests for conversations

---
+++ tests/conversationTest.js
@@ -0,0 +1,88 @@
+class Graph {
+  constructor(initialDialogue) {
+    this.noOfVertices = 0;
+    this.AdjList = new Map();
+
+    if (initialDialogue) {
+      for (let i = 0; i < initialDialogue.length; i++) {
+        this.addVertex(initialDialogue[i][0], initialDialogue[i][1])
+      }
+    }
+  }
+
+  addVertex(id, dialogue = "null") {
+    this.noOfVertices++;
+    this.AdjList.set(id, { dialogue: dialogue, choice: [] });
+  }
+
+  addEdge(id, next, choice = null) {
+    let value = this.AdjList.get(id);
+    value.choice.push({ to: next, choice: choice });
+
+    //for undirected
+    //this.Adjlist.get(w).push(v);
+  }
+
+  getChoices(id) {
+    let options = this.AdjList.get(id).choice;
+    return options;
+  }
+
+  getCurrentText(id) {
+    let currentText = this.AdjList.get(id).dialogue;
+    return currentText;
+  }
+
+  printGraph() {
+    var get_keys = this.AdjList.keys();
+
+    for (var i of get_keys) {
+
+      var get_values = this.AdjList.get(i).choice;
+      var conc = "";
+
+      for (var j of get_values) {
+        conc += j.to + " ";
+      }
+
+      console.log(i + " => " + conc);
+    }
+  }
+}
+
+
+module.exports = Graph;
+
+
+// var vertices = [
+//   ["A", "A_TEXT"], ["B", "B_TEXT"], ["C", "C_TEXT"], ["D", "D_TEXT"], ["E", "E_TEXT"], ["F", "F_TEXT"]
+// ]
+// var g = new Graph(vertices);
+
+// // adding vertices 
+// // for (var i = 0; i < vertices.length; i++) {
+// //   g.addVertex(vertices[i]);
+// // }
+
+// // adding edges 
+// g.addEdge('A', 'B', "Greetings Adventurer");
+// g.addEdge('A', 'D', "You will perish");
+// g.addEdge('A', 'E', "Goodbye");
+// g.addEdge('B', 'C');
+// g.addEdge('D', 'E');
+// g.addEdge('E', 'F');
+// g.addEdge('E', 'C');
+// g.addEdge('C', 'F');
+
+// // prints all vertex and 
+// // its adjacency list 
+// // A -> B D E 
+// // B -> A C 
+// // C -> B E F 
+// // D -> A E 
+// // E -> A D F C 
+// // F -> E C 
+// // g.printGraph();
+// // console.log(g.getChoices("A"));
+// console.log(g.getCurrentText("A"));
+// console.log(g.getChoices("A"));
---
+++ tests/test.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
+const Conversation = require('./conversationTest.js');
+
+
+var textDialogues = [["ROOT", "Well hello there"], ["A", "I hope you have a nice day"], ["B", "HOW DARE YOU SAY SUCH A THING, GET OUT!!!"], ["C", "Goodbye"]];
+let g = new Conversation(textDialogues);
+g.addEdge("ROOT", "A", "I think I should probably get going...");
+g.addEdge("ROOT", "B", "Your mother was a hamster, and your father smelled of elderberries!");
+g.addEdge("A", "C", "You too. Goodbye!");
+
+console.log(assert);
+
+
+
+
+describe('Basic Conversation', function () {
+  beforeEach(function () {
+    var textDialogues = [["ROOT", "Well hello there"], ["A", "I hope you have a nice day"], ["B", "HOW DARE YOU SAY SUCH A THING, GET OUT!!!"], ["C", "Goodbye"]];
+    var g = new Conversation(textDialogues);
+    g.addEdge("ROOT", "A", "I think I should probably get going...");
+    g.addEdge("ROOT", "B", "Your mother was a hamster, and your father smelled of elderberries!");
+    g.addEdge("A", "C", "You too. Goodbye!");
+  })
+
+  describe('Basic Methods', function () {
+    it('should return the following options at ROOT', function () {
+      let choicesArr = g.getChoices("ROOT");
+      let choices = [];
+      choicesArr.forEach((val) => { choices.push(val.choice) });
+
+      expect(choices).to.include('I think I should probably get going...')
+      expect(choices).to.include('Your mother was a hamster, and your father smelled of elderberries!')
+      assert.equal(choices.length, 2);
+    });
+
+    it('should include the following options at node A', function () {
+      let choices = g.getChoices("A");
+      assert.include(choices, "Goodbye");
+      assert.equal(choices.length, 1);
+    })
+  });
+});

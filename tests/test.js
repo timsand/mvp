6701c29cc66e1206d7576826935ca4b6ca66cbe4
Tim Sanderson <twsand11@gmail.com>
2020-02-06 14:05:23 -0600

    removed unnecessary code

--- tests/test.js
+++ tests/test.js
@@ -4,15 +4,6 @@ var expect = chai.expect;
 const Conversation = require('./conversationTest.js');
 
 
-var textDialogues = [["ROOT", "Well hello there"], ["A", "I hope you have a nice day"], ["B", "HOW DARE YOU SAY SUCH A THING, GET OUT!!!"], ["C", "Goodbye"]];
-let g = new Conversation(textDialogues);
-g.addEdge("ROOT", "A", "I think I should probably get going...");
-g.addEdge("ROOT", "B", "Your mother was a hamster, and your father smelled of elderberries!");
-g.addEdge("A", "C", "You too. Goodbye!");
-
-console.log(assert);
-
-
 
 
 describe('Basic Conversation', function () {
